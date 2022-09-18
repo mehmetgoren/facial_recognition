52ca3ef1d90d1a228c8067d15de3c750b5cd51e0
gokalp <mehmetgoren@outlook.com>
2022-09-19 02:46:03 +0300

    Fixes broken redis connection issue on heavy load situation

--- main_pytorch.py
+++ main_pytorch.py
@@ -1,3 +1,5 @@
+import logging
+import time
 import warnings
 
 from common.event_bus.event_bus import EventBus
@@ -22,9 +24,20 @@ def main():
     handler = ReadServiceEventHandler()
     handler.start_listen_train_complete_event()
 
+    logger.setLevel(logging.INFO)
+    logging.basicConfig(level=logging.INFO)
     logger.info('pytorch face recognition service will start soon')
-    event_bus = EventBus(EventChannels.read_service)
-    event_bus.subscribe_async(handler)
+
+    def listen_event_bus():
+        try:
+            event_bus = EventBus(EventChannels.read_service)
+            event_bus.subscribe_async(handler)
+        except BaseException as ex:
+            logger.error(f'an error occurred while listening read service handler, ex: {ex}')
+            time.sleep(1.)
+            listen_event_bus()
+
+    listen_event_bus()
 
 
 if __name__ == '__main__':
